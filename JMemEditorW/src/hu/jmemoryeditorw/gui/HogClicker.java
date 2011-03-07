f1cbd08ec948fe3556bae90864a9a99ee892fe9e
akarnokd <akarnokd@gmail.com>
2011-03-07 10:09:38 +0000

    some timing fixes and a screenshot

--- JMemEditorW/src/hu/jmemoryeditorw/gui/HogClicker.java
+++ JMemEditorW/src/hu/jmemoryeditorw/gui/HogClicker.java
@@ -680,9 +680,12 @@ public class HogClicker extends JFrame {
 		Rect r = new Rect();
 		User32.INSTANCE.GetWindowRect(wnd, r);
 		try {
+			Thread.sleep(500);
 			User32.INSTANCE.SetForegroundWindow(wnd);
+			Thread.sleep(250);
 			Robot robot = new Robot();
 			BufferedImage img = robot.createScreenCapture(new Rectangle(r.left, r.top, r.right - r.left + 1, r.bottom - r.top + 1));
+			Thread.sleep(250);
 			main.image = img;
 			zoom.image = img;
 			popupZoom.image = img;
@@ -692,6 +695,8 @@ public class HogClicker extends JFrame {
 			repaint();
 			popupZoom.repaint();
 		} catch (AWTException ex) {
+			ex.printStackTrace();
+		} catch (InterruptedException ex) {
 			ex.printStackTrace();
 		} finally {
 			statusLineToLastError();
